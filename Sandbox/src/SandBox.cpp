d6c0327b0fff2470559a241de346b1738962e36b
Archil Mezvrishvili <a.mezvrishvili@adjarabet.com>
2024-01-30 00:05:42 +0400

    Push to  git

---
+++ Sandbox/src/SandBox.cpp
@@ -0,0 +1,22 @@
+#include <Hazel/Hazel.h>
+
+class  Sandbox : public  Hazel::Application
+{
+public: 
+	Sandbox()
+	{
+
+	}
+
+	~Sandbox()
+	{
+
+	}
+};
+
+void main()
+{
+	Sandbox* sandbox = new Sandbox();
+	sandbox->Run();
+	delete sandbox;
+}
